a8194c06ca553a38032a0f5691d2d637f27f4f55
hp <vikashpcst86@gmail.com>
2019-12-25 00:07:34 -0800

    improve navigation bar 3

--- app/src/main/java/com/delhidairy/RegistrationFormActivity.java
+++ app/src/main/java/com/delhidairy/RegistrationFormActivity.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrationFormActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registration_form_activity);
     }
 }
 
