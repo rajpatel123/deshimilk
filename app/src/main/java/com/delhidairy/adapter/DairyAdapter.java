9a718186201878363ed2fa33ae008771b766b587
shubhanshu <shubhanshukumar2811@gmail.com>
2019-12-25 18:38:58 +0530

    adding model class and api images with background color

--- app/src/main/java/com/delhidairy/adapter/DairyAdapter.java
+++ app/src/main/java/com/delhidairy/adapter/DairyAdapter.java
@@ -1,10 +1,12 @@
 package com.delhidairy.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.delhidairy.R;
+import com.delhidairy.activities.CowMilkActivity;
 import com.delhidairy.model.Record;
 import com.delhidairy.utils.Constants;
 import com.squareup.picasso.Picasso;
@@ -19,6 +22,8 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 public class DairyAdapter extends RecyclerView.Adapter<DairyAdapter.ProgrammingViewholder> {
+
+    Button btnTitle;
 
     Context context;
     private List<Record> data;
@@ -46,7 +51,14 @@ public class DairyAdapter extends RecyclerView.Adapter<DairyAdapter.ProgrammingV
     @Override
     public void onBindViewHolder(@NonNull ProgrammingViewholder holder, int position) {
         Record records = data.get(position);
-        holder.txtTitle.setText(records.getProductname());
+        holder.btnTitle.setText(records.getProductname());
+        holder.btnTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(context, CowMilkActivity.class);
+                context.startActivity(intent);
+            }
+        });
         Picasso.with(context).load(Constants.BASE_URL+records.getProductimage());
         Log.d("Init","onBindViewHolder");
 
@@ -76,12 +88,12 @@ public class DairyAdapter extends RecyclerView.Adapter<DairyAdapter.ProgrammingV
     public class ProgrammingViewholder extends RecyclerView.ViewHolder{
 
         ImageView imgIcon;
-        TextView txtTitle;
+        Button btnTitle;
         public ProgrammingViewholder(@NonNull View itemView) {
             super(itemView);
 
             imgIcon =(ImageView) itemView.findViewById(R.id.imgIcon);
-            txtTitle = (TextView) itemView.findViewById(R.id.txtTitle);
+            btnTitle = (Button) itemView.findViewById(R.id.btnTitle);
 
         }
     }
